72e579c5aad13d5c9f9b13e1b99490f2f0bb84ca
Zul <zulkifl.alidou@gmail.com>
2024-06-28 16:06:08 -0700

    feat: implement the File > Close button

--- src/main/java/com/example/notepadsharp/MainController.java
+++ src/main/java/com/example/notepadsharp/MainController.java
@@ -1,14 +1,12 @@
 package com.example.notepadsharp;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class MainController {
     @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void closeApp() {
+        Platform.exit();
     }
 }
